1a1ea9641858dbbf9333268f6e2e5f5f31adcea4
Kyle Becker <kmb08625@uga.edu>
2025-03-24 16:24:40 -0400

    Add MessageField

---
+++ frontend/src/components/MessageField.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useRef } from 'react';
+import { Flex, Text } from '@chakra-ui/react';
+
+interface Message {
+  role: string;
+  content: string;
+}
+interface MessageList {
+  messages: Message[];
+}
+
+export default function MessageField({ messages }: MessageList) {
+  const AlwaysScrollToBottom = () => {
+    const elementRef = useRef<HTMLDivElement>(null);
+    useEffect(() => elementRef.current?.scrollIntoView());
+    return <div ref={elementRef} />;
+  }
+
+  return (
+    <Flex w='100%' h='80%' overflowY='scroll' flexDir='column'>
+      {messages.map((item, index) => {
+        if (item.role === 'user') {
+          return (
+            <Flex key={index} w='100%' p={2} bg='blue.200' justify='flex-end'>
+              <Text color='white'>{item.content}</Text>
+            </Flex>
+          );
+        } else {
+          return (
+            <Flex key={index} w='100%' p={2} bg='gray.100'>
+              <Text>{item.content}</Text>
+            </Flex>
+          );
+        }
+      })}
+      <AlwaysScrollToBottom />
+    </Flex>
+  );
+}
