5516bc513be84ca91619f4da0c267a81d6efa608
Kyle Becker <kmb08625@uga.edu>
2025-03-24 16:40:43 -0400

    Add dummy message field

--- frontend/src/pages/ChatPage.tsx
+++ frontend/src/pages/ChatPage.tsx
@@ -1,14 +1,48 @@
+import { useState, } from 'react';
 import { Flex, Heading, Box, Separator } from '@chakra-ui/react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import MessageField from '../components/MessageField';
+
+interface Message {
+  role: string;
+  content: string;
+}
 
 export default function ChatPage() {
+  const [messages, setMessages] = useState([
+    { role: "computer", content: "Hi, My Name is HoneyChat" },
+    { role: "user", content: "Hey there" },
+    { role: "user", content: "Hi, I'm Kyle." },
+    {
+      role: "computer",
+      content: "Nice to meet you. You can send me message and i'll reply you with same message.",
+    },
+  ]);
+  const [inputMessage, setInputMessage] = useState("");
+
+  const handleSendMessage = () => {
+    if (!!inputMessage.trim().length) {
+      const data = inputMessage;
+
+      // Add user message to message list
+      setMessages((old) => [...old, { role: 'user', content: data }]);
+      setInputMessage("");
+
+      // Echo user message with computer message
+      // TODO: Implement Query chatbot here
+      setTimeout(() => {
+        setMessages((old) => [...old, { role: 'computer', content: data }]);
+      }, 1000);
+    }
+  };
+
   return (
     <Flex w='100%' h='100vh' justify='center' align='center' border='5px solid red'>
       <Flex w='80%' h='90%' flexDir='column' border='2px solid blue'>
         <Header />
         <Separator />
-        {/* Chat components here */}
+        <MessageField messages={messages} />
         <Footer />
       </Flex>
     </Flex>
