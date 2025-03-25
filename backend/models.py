5b31b608e7e7879244f85402dbb5304580082d30
Kyle Becker <kmb08625@uga.edu>
2025-03-25 17:02:39 -0400

    Set up models

---
+++ backend/models.py
@@ -0,0 +1,12 @@
+from sqlmodel import SQLModel, Field, Session, create_engine, select
+from uuid import uuid4
+
+
+class ConversationThread(SQLModel, table=True):
+    id: str = Field(default_factory=lambda: str(uuid4()), primary_key=True)
+
+class Message(SQLModel, table=True):
+    id: str = Field(default_factory=lambda: str(uuid4()), primary_key=True)
+    role: str
+    content: str
+    thread_id: str = Field(foreign_key='conversationthread.id')
