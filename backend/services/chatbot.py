b626841ed62f4524d5df42e3a8229da7d6ac345e
Kyle Becker <kmb08625@uga.edu>
2025-03-27 13:58:22 -0400

    Give chatbot a settable name

--- backend/services/chatbot.py
+++ backend/services/chatbot.py
@@ -1,9 +1,20 @@
+from typing import Sequence, Annotated, TypedDict
+
 from langchain.chat_models import init_chat_model
 from langgraph.checkpoint.memory import MemorySaver
-from langgraph.graph import START, MessagesState, StateGraph
+from langgraph.graph import START, MessagesState, StateGraph, add_messages
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-from langchain_core.messages import HumanMessage, AIMessage, trim_messages
+from langchain_core.messages import BaseMessage, HumanMessage, AIMessage, trim_messages
 from uuid import uuid4, UUID
+
+
+CHAT_NAME = 'HoneyChat'
+
+
+class State(TypedDict):
+    messages: Annotated[Sequence[BaseMessage], add_messages]
+    name: str
+
 
 # Initialize the chat model
 model = init_chat_model('gpt-4o-mini', model_provider='openai')
@@ -13,11 +24,11 @@ prompt_template = ChatPromptTemplate.from_messages(
     [
         (
             'system',
-            'You are a helpful AI assistant named HoneyChat. Answer all questions to the best of your ability.'
+            'You are a helpful AI assistant named {name}. Answer all questions to the best of your ability.'
         ),
         (
             'assistant',
-            'Hi, My name is HoneyChat'
+            'Hi, My name is {name}'
         ),
         MessagesPlaceholder(variable_name='messages'),
     ]
@@ -35,15 +46,15 @@ trimmer = trim_messages(
 
 
 # Function to call model
-def call_model(state: MessagesState):
+def call_model(state: State):
     trimmed_msg = trimmer.invoke(state['messages'])
-    prompt = prompt_template.invoke({'messages': trimmed_msg})
+    prompt = prompt_template.invoke({'messages': trimmed_msg, 'name': state['name']})
     response = model.invoke(prompt)
-    return {'messages': response}
+    return {'messages': [response]}
 
 
 # Initialize the state graph
-workflow = StateGraph(state_schema=MessagesState)
+workflow = StateGraph(state_schema=State)
 workflow.add_edge(START, 'model')
 workflow.add_node('model', call_model)
 
@@ -53,9 +64,9 @@ app = workflow.compile(checkpointer=MemorySaver())
 # Function to return AIMessage
 def get_ai_message(query: str, thread_id: UUID | str) -> AIMessage:
     input_messages = [HumanMessage(query)]
-    output = app.invoke({'messages': input_messages}, {'configurable': {'thread_id': thread_id}})
+    output = app.invoke({'messages': input_messages, 'name': CHAT_NAME}, {'configurable': {'thread_id': thread_id}})
     return output['messages'][-1]
 
 
 def invoke_model(input_messages, thread_id):
-    return app.invoke({'messages': input_messages}, {'configurable': {'thread_id': thread_id}})
+    return app.invoke({'messages': input_messages, 'name': CHAT_NAME}, {'configurable': {'thread_id': thread_id}})
